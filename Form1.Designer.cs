8105b3f217160b017d9d0af382fcfeb5f4b12f01
roro11fr <94123839+roro11fr@users.noreply.github.com>
2023-07-16 01:40:59 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace quizInterface
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblQuestion1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblQuestion1
+            // 
+            this.lblQuestion1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQuestion1.Location = new System.Drawing.Point(-22, 104);
+            this.lblQuestion1.Name = "lblQuestion1";
+            this.lblQuestion1.Size = new System.Drawing.Size(457, 144);
+            this.lblQuestion1.TabIndex = 1;
+            this.lblQuestion1.Text = "Question1";
+            this.lblQuestion1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.lblQuestion1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(123, 318);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(234, 54);
+            this.button1.TabIndex = 2;
+            this.button1.Tag = "1";
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.checkAnswer);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(415, 318);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(234, 54);
+            this.button2.TabIndex = 3;
+            this.button2.Tag = "2";
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.checkAnswer);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(123, 384);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(234, 54);
+            this.button3.TabIndex = 4;
+            this.button3.Tag = "3";
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.checkAnswer);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(415, 384);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(234, 54);
+            this.button4.TabIndex = 5;
+            this.button4.Tag = "4";
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.checkAnswer);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::quizInterface.Properties.Resources.descărcare;
+            this.pictureBox1.Location = new System.Drawing.Point(518, 46);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(281, 233);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblQuestion1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblQuestion1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace quizInterface
+{
+    public partial class Form1 : Form
+    {
+        // quiz game variables
+
+        int correctAnswer;
+        int questionNumber = 1;
+        int score, percentage, totalQuestions;
+        public Form1()
+        {
+            InitializeComponent();
+
+            askQuestion(questionNumber);
+
+            totalQuestions = 10;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+      
+        }
+
+        private void checkAnswer(object sender, EventArgs e)
+        {
+            var senderObject = (Button)sender;
+
+            int buttonTag = Convert.ToInt32(senderObject.Tag);
+
+            if (buttonTag == correctAnswer)
+            {
+                score++;
+            }
+
+            if (questionNumber == totalQuestions)
+            {
+                percentage = (int)Math.Round((double)(score * 100) / totalQuestions);
+
+                MessageBox.Show(
+                    "Quiz Ended!" + Environment.NewLine +
+                    "AI raspuns" + score + "atatea intrebari corecte" + Environment.NewLine +
+                    "Procentul tau este" + percentage + Environment.NewLine +
+                    "Click to play again"
+                    );
+
+                score = 0;
+                questionNumber = 0;
+                askQuestion(questionNumber);
+            }
+
+            questionNumber++;
+            askQuestion(questionNumber);
+        }
+
+        private void askQuestion (int gnum)
+        {
+            switch (gnum)
+            {
+                case 1:
+                    pictureBox1.Image = Properties.Resources.descărcare;
+
+                    lblQuestion1.Text = "Ce echipa este echipa din imagine?";
+
+                    button1.Text = "MAnchester City";
+                    button2.Text = "Bayern Munchen";
+                    button3.Text = "Manchester UNited";
+                    button4.Text = "Manchester United";
+
+                    correctAnswer = 4;
+
+                    break;
+
+                case 2:
+                    pictureBox1.Image = Properties.Resources.balonD_or;
+
+                    lblQuestion1.Text = "Care este fotbalistul cu cele mai multe balone de aur?";
+
+                    button1.Text = "Lionel Pessi";
+                    button2.Text = "Cristiano Ronaldo";
+                    button3.Text = "Pele";
+                    button4.Text = "Maradona";
+
+                    correctAnswer = 1;
+
+                    break;
+
+                case 3:
+                    pictureBox1.Image = Properties.Resources.bani;
+
+                    lblQuestion1.Text = "Care este fotbalistul cel mai scump din istorie?";
+
+                    button1.Text = "Neymar Jr";
+                    button2.Text = "Mbappe";
+                    button3.Text = "Ronaldo";
+                    button4.Text = "Messi";
+
+                    correctAnswer = 1;
+
+                    break;
+
+                case 4:
+                    pictureBox1.Image = Properties.Resources.champleague1;
+
+                    lblQuestion1.Text = "Cine este denumit Mister CHampions League?";
+
+                    button1.Text = "Messi";
+                    button2.Text = "Cristiano Ronaldo";
+                    button3.Text = "Zidane";
+                    button4.Text = "Benzema";
+
+                    correctAnswer = 2;
+
+                    break;
+
+                case 5:
+                    pictureBox1.Image = Properties.Resources.champleague2;
+
+                    lblQuestion1.Text = "Care echipa are cele mai multe trofee Champ League?";
+
+                    button1.Text = "Fc Varcelona";
+                    button2.Text = "MAn Utd";
+                    button3.Text = "Ac Milan";
+                    button4.Text = "Ream Madrid";
+
+                    correctAnswer = 4;
+
+                    break;
+
+                case 6:
+                    pictureBox1.Image = Properties.Resources.goldenboy;
+
+                    lblQuestion1.Text = "Cine a fost denumit Golden Boy in 2015?";
+
+                    button1.Text = "Messi";
+                    button2.Text = "Mbappe";
+                    button3.Text = "Martial";
+                    button4.Text = "Renato Sanches";
+
+                    correctAnswer = 3;
+
+                    break;
+
+                case 7:
+                    pictureBox1.Image = Properties.Resources.dubai;
+
+                    lblQuestion1.Text = "Care este fotbalistul cu cele mai multe d'or uri?";
+
+                    button1.Text = "Ronaldo";
+                    button2.Text = "Messi";
+                    button3.Text = "Zidane";
+                    button4.Text = "Roaldinho";
+
+                    correctAnswer = 1;
+
+                    break;
+
+                case 8:
+                    pictureBox1.Image = Properties.Resources.ozil;
+
+                    lblQuestion1.Text = "La ce echipa nu a jucat Mesut Ozil";
+
+                    button1.Text = "Fenerbache";
+                    button2.Text = "Real Madrid";
+                    button3.Text = "Arsenal";
+                    button4.Text = "Bayern Munchen";
+
+                    correctAnswer = 4;
+
+                    break;
+
+                case 9:
+                    pictureBox1.Image = Properties.Resources.balonD_or;
+
+                    lblQuestion1.Text = "Care este tara cu cele mai multe world cup uri?";
+
+                    button1.Text = "Argentina";
+                    button2.Text = "Romania";
+                    button3.Text = "Germania";
+                    button4.Text = "Brazilia";
+
+                    correctAnswer = 4;
+
+                    break;
+
+                case 10:
+                    pictureBox1.Image = Properties.Resources.gunners;
+
+                    lblQuestion1.Text = "Ce echipa este denumite 'The Gunners'?";
+
+                    button1.Text = "Arsenal";
+                    button2.Text = "Man Utd";
+                    button3.Text = "Barcelona";
+                    button4.Text = "Spurs";
+
+                    correctAnswer = 1;
+
+                    break;
+            }
+        }
+    }
+}
